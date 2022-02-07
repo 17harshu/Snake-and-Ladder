31092ea4e0455191dfed0f97d3fd3943b1db57fa
17harshu <harshalanandas17@gmail.com>
2022-02-07 22:46:10 +0530

    added: noplay, snake, ladder

--- src/com/snakeladder/problems/SnakeLadderGame.java
+++ src/com/snakeladder/problems/SnakeLadderGame.java
@@ -1,15 +1,54 @@
 package com.snakeladder.problems;
 
+import java.util.Random;
+
 public class SnakeLadderGame {
+	static final int noPlay=0;
+	static final int ladder=1;
+	static final int snake=2;
 	
 	public static void rollDice() {
 		int dice=(int) (Math.floor(Math.random() * 10) % 6) + 1;
 		System.out.println("Dice rolled value: " + dice);
 	}
 	
+	public static void checkOption() {
+		  //Random ran = new Random();
+		  //int dice = ran.nextInt(6)+1;
+		  //int position = 0;
+		  //int ran=(int) (Math.floor(Math.random() * 10) % 6) + 1;
+		 int position = 0;
+		  Random ran = new Random();
+		  int dice = ran.nextInt(6)+1;
+		  System.out.println("dice: "+dice);
+		int optionCheck = ran.nextInt(3);
+		  System.out.println("optionCheck: "+optionCheck);
+		  
+		  if (optionCheck == ladder) 
+		  {
+		   System.out.println("Ladder");
+		   position = position + dice;
+		  }
+		  
+		  else if (optionCheck == snake) 
+		  {
+		   System.out.println("Snake");
+		   position = position - dice;
+		  }
+		  else {
+		   System.out.println("No Play");
+		  }
+		  if (position<0)
+		   {
+			  position=0;
+		   }
+		   System.out.println("position: "+position);
+	}
+	
 	public static void main(String[] Args) {
         int start = 0;
         System.out.println("Start Position:" +start);
         rollDice();
+        checkOption();
     }
 }
