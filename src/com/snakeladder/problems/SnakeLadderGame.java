3fcdec49e2ba267a1a35c5e31bbc82fab9f54e00
17harshu <harshalanandas17@gmail.com>
2022-02-07 20:57:58 +0530

    added: dice rolled

--- src/com/snakeladder/problems/SnakeLadderGame.java
+++ src/com/snakeladder/problems/SnakeLadderGame.java
@@ -1,8 +1,15 @@
 package com.snakeladder.problems;
 
 public class SnakeLadderGame {
+	
+	public static void rollDice() {
+		int dice=(int) (Math.floor(Math.random() * 10) % 6) + 1;
+		System.out.println("Dice rolled value: " + dice);
+	}
+	
 	public static void main(String[] Args) {
         int start = 0;
         System.out.println("Start Position:" +start);
+        rollDice();
     }
 }
