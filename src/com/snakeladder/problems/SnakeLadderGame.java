3e9b59a3a144fbb76c86835d603a98f60bad7866
17harshu <harshalanandas17@gmail.com>
2022-02-07 20:21:38 +0530

    added: starting position

---
+++ src/com/snakeladder/problems/SnakeLadderGame.java
@@ -0,0 +1,8 @@
+package com.snakeladder.problems;
+
+public class SnakeLadderGame {
+	public static void main(String[] Args) {
+        int start = 0;
+        System.out.println("Start Position:" +start);
+    }
+}
