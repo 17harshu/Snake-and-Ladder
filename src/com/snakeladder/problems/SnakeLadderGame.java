631491c244840e193147f704090df6a5a999ea20
17harshu <harshalanandas17@gmail.com>
2022-02-08 22:20:34 +0530

    added: dice count to win game

--- src/com/snakeladder/problems/SnakeLadderGame.java
+++ src/com/snakeladder/problems/SnakeLadderGame.java
@@ -111,6 +111,41 @@ public class SnakeLadderGame {
 		}
 	}
 	
+	public static void winGAme() {
+		int position = 0;
+		while (position<100) 
+		{
+			Random ran = new Random();
+			int dice = ran.nextInt(6)+1;
+			System.out.println("dice: "+dice);
+			++dice;
+			int optionCheck = ran.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck);
+			
+			
+			if ((optionCheck == ladder) && (position+dice)<=100) 
+			{
+				System.out.println("ladder");
+				position = position + dice;
+			}
+			else if (optionCheck == snake) 
+			{
+				System.out.println("Snake");
+				position = position - dice;
+			}
+			else {
+				System.out.println("Noplay");				
+			}
+			if (position<0) 
+			{
+				position =0;
+			}
+			System.out.println("position: "+position+"\ndicecount: "+dice);
+			
+		}
+	}
+	
+	
 	public static void main(String[] Args) {
         int start = 0;
         System.out.println("Start Position:" +start);
