4d2d7da7f3c3da0531fba1fe4cfebe1fa7a9acdf
17harshu <harshalanandas17@gmail.com>
2022-02-08 22:32:08 +0530

    added: 2 player

--- src/com/snakeladder/problems/SnakeLadderGame.java
+++ src/com/snakeladder/problems/SnakeLadderGame.java
@@ -145,7 +145,68 @@ public class SnakeLadderGame {
 		}
 	}
 	
+	public static void twoPlayer() {
+		final int snake = 2;
+		final int ladder = 1;
+		final int Noplay= 0;
+		int diceCount = 0;
+		final int PLAYER1 = 1;
+		final int PLAYER2 = 2;
+		int playerOnePosition = 0;
+		int playerTwoPosition = 0;
+		int player = PLAYER1;
+		System.out.println("playerOnePosition: "+playerOnePosition);
+		System.out.println("playerTwoPosition: "+playerTwoPosition);
+		Random ran = new Random();
+		
+		while ((playerOnePosition<100) && (playerTwoPosition<100)) 
+		{
+			
+			int dice = ran.nextInt(6)+1;
+			System.out.println("dice: "+dice);
+			++diceCount;
+			int optionCheck = ran.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck);
+			
+			if(player == PLAYER1) 
+			{
+			
+				playerOnePosition=positionCheck(dice,playerOnePosition,optionCheck);
+				if ((optionCheck == snake) || (optionCheck == Noplay)) 
+				{
+					player = PLAYER2;
+				}									
+			}
+			else if (player == PLAYER2) {
+				playerTwoPosition=positionCheck(dice,playerTwoPosition,optionCheck);
+				if ((optionCheck == snake) || (optionCheck == Noplay)) 
+				{
+					player = PLAYER1;
+				}
+			}
+						
+		}
+		System.out.println();
+		System.out.println("Player One Position: "+playerOnePosition);
+		System.out.println("Player Two Position"+playerTwoPosition);
+		System.out.println("diceCount: "+diceCount);
+		if(playerOnePosition == 100) 
+		{
+			System.out.println("Player One Wins");
+		}
+		else 
+		{
+			System.out.println("Player Two Wins");
+		}
 	
+	}
+	
+	
+	private static int positionCheck(int dice, int playerTwoPosition, int optionCheck) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	public static void main(String[] Args) {
         int start = 0;
         System.out.println("Start Position:" +start);
@@ -153,5 +214,6 @@ public class SnakeLadderGame {
         checkOption();
         winningPosition();
         checkWinningPosition();
+        twoPlayer();
     }
 }
